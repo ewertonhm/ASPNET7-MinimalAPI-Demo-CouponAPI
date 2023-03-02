4be54f695f347aa7c3a61c087fe18266bcb091ab
Ewerton Henrique Marschalk <ewerton.henrique@havan.com.br>
2023-03-02 00:55:41 -0300

    cleaning unused code from the program.cs

--- CouponAPI/Program.cs
+++ CouponAPI/Program.cs
@@ -1,36 +1,23 @@
-using AutoMapper;
 using CouponAPI;
-using CouponAPI.Data;
 using CouponAPI.Endpoints;
-using CouponAPI.Models;
-using CouponAPI.Models.Dto;
 using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Dependency Injection
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-
 var app = builder.Build();
 
-app.MapHealthChecks("/health");
-
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapHealthChecks("/health");
 app.ConfigureCouponEndpoints();
 app.UseHttpsRedirection();
 app.Run();
